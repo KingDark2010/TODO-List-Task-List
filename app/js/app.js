58f720c6f792ec96f49290f258320369c7acc1eb
KingDark2010 <ahmed-ahmed83356@hotmail.com>
2021-07-27 17:54:36 +0200

    adding delete task function

--- app/js/app.js
+++ app/js/app.js
@@ -1,19 +1,14 @@
 // Define UI Vars
 
 const taskInput = document.querySelector('#exampleFormControlInput1');
-console.log(taskInput.nextSibling);
 
 const taskAddBtn = document.querySelector('.btn.btn-outline-success.btn-sm');
 
 const taskFilter = document.querySelector('#exampleFormControlInput2');
 
-const taskDeleteAll = document.querySelector('.btn.btn-outline-danger.btn-lg');
+const taskDeleteAll = document.querySelector('#delete-all');
 
 const taskCollection = document.querySelector('#task-collection');
-console.log(taskAddBtn);
-
-// alert creation
-
 
 // Load all events
 loadEventListener()
@@ -21,11 +16,17 @@ loadEventListener()
 
 // Load all events function
 function loadEventListener(){
+    // add task
     taskAddBtn.addEventListener('click', addTask);
+    // add task with enter key
     taskInput.addEventListener("keyup", enterTask);
+    // delete single task
+    taskCollection.addEventListener("click", removeTask);
+    // delete all tasks
+    taskDeleteAll.addEventListener("click", removeAllTasks);
 }
 
-// add item on click
+// add item on click function
 
 function addTask(event) {
     if (taskInput.value === "") {
@@ -38,7 +39,7 @@ function addTask(event) {
         // adding task to table
         task.innerHTML = `${taskInput.value}`;
         taskDelete.classList= 'left-btn'
-        taskDelete.innerHTML = `<button type="button" class="btn btn-outline-danger"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+        taskDelete.innerHTML = `<button type="button" class="btn btn-outline-danger delete"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
         <path d="M1.293 1.293a1 1 0 0 1 1.414 0L8 6.586l5.293-5.293a1 1 0 1 1 1.414 1.414L9.414 8l5.293 5.293a1 1 0 0 1-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L6.586 8 1.293 2.707a1 1 0 0 1 0-1.414z"/>
         </svg> Delete</button>`
         taskRaw.appendChild(task);
@@ -50,10 +51,35 @@ function addTask(event) {
     event.preventDefault()
 }
 
-// make tasks adable woith enter key 
+// make tasks adable woith enter key function
 function enterTask(event) {
     if (event.key == "Enter") {
         taskAddBtn.click();
     }
     event.preventDefault();
+}
+
+// remove choosen task function
+
+function removeTask(event) {
+    if(event.target.classList.contains('delete')) {
+        if(confirm("are you sure ?")) {
+            event.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+//remove all tasks 
+
+function removeAllTasks() {
+    while(taskCollection.firstChild) {
+        taskCollection.removeChild(taskCollection.firstChild);
+    }
+}
+
+
+// filter task function
+
+function filterTasks(event) {
+    const text = event.target.value.toLowerCase();
 }
